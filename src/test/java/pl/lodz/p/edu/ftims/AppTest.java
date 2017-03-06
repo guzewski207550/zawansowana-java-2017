8cf2d5a273e9a9946c373ab67dcdb5f2ced9da24
MAKOLAB-NET\mateusz.guzewski <mateusz.guzewski@makolab.com>
2017-03-06 14:02:38 +0100

    Remove default jUnit test method.

--- src/test/java/pl/lodz/p/edu/ftims/AppTest.java
+++ src/test/java/pl/lodz/p/edu/ftims/AppTest.java
@@ -1,41 +1,12 @@
 package pl.lodz.p.edu.ftims;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.*;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest extends TestCase {
+import org.junit.Test;
 
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest(String testName) {
-        super(testName);
-    }
+public class AppTest {
 
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite() {
-        return new TestSuite(AppTest.class);
-    }
-
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp() {
-        assertTrue(true);
-    }
-
-
-    @org.junit.Test
+    @Test
     public void TmpClassTest() {
         int value = 5;
         TmpClass tmpClass = new TmpClass();
