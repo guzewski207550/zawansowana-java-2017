e105b8acbc1f1679724f02c6e3b9c3f68fec3517
MAKOLAB-NET\mateusz.guzewski <mateusz.guzewski@makolab.com>
2017-03-28 20:02:25 +0200

    Upload Step, SimpleStep and complexStep class.

---
+++ generics-collections/src/main/java/pl/lodz/p/edu/ftims/singly/SimpleStep.java
@@ -0,0 +1,16 @@
+package pl.lodz.p.edu.ftims.singly;
+
+public class SimpleStep extends Step {
+
+    @Override
+    public String getDescription() {
+        return super.getDescription();
+    }
+
+
+    @Override
+    public void setDescription(String description) {
+        super.setDescription(description);
+    }
+
+}
